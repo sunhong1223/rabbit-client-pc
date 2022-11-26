8879be638ca4936c790627c8b91b2d432e8006be
sunhong1223 <845036815@qq.com>
2022-11-26 22:34:46 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- vue2.0 根元素；vue3.0 可以是代码片段 -->
+  <p>根组件</p>
+  <div>App</div>
+</template>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+// vue2.0 new Vuex.Store({}) 创建仓库
+// vue3.0 createStore({}) 创建仓库
+export default createStore({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
