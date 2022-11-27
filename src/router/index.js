139ea8115e48a1f47b3312e476bfad34467553c5
sunhong1223 <845036815@qq.com>
2022-11-28 03:57:08 +0800

    首页路由和组件

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,10 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 // 路由规则
 const routes = [
+  // 首页
   {
     path: '/',
-    component: () => import('@/views/Layout'),
+    component: () => import('@/views/Layout'), // 路由懒加载：动态导入，路由被访问的时候才加载对应组件
     children: [
       {
         path: '/',
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -14,7 +14,7 @@
 
 <script>
 export default {
-  name: 'LayoutPage'
+  name: 'xtx-layout'
 }
 </script>
 
